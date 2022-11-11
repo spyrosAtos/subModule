4959f923fdf6416551f5568ed8704073f15c2925
spyrosAtos <spyros.chasakos@atos.net>
2022-11-11 13:30:02 +0200

    Added TC template

---
+++ src/test/java/org/example/MainTest.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import static org.testng.Assert.*;
+
+public class MainTest {
+
+    @BeforeMethod
+    public void setUp() {
+    }
+
+    @AfterMethod
+    public void tearDown() {
+    }
+}
